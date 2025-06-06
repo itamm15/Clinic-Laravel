74bb85394d57d1c8b130031fb32ce012f41cae73
itamm15 <osinski.mateusz15@gmail.com>
2025-06-06 22:54:47 +0200

    chore: list patients

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Patient;
 
 class PatientController extends Controller
 {
     public function index()
     {
-        return view('patients.index');
+        $patients = Patient::with('user')->get();
+        return view('patients.index', compact('patients'));
     }
 }
--- resources/views/patients/index.blade.php
+++ resources/views/patients/index.blade.php
@@ -1,5 +1,24 @@
 @extends('layout')
 
 @section('content')
-<h1>Pacjenci</h1>
+  <h1>Lista pacjentów</h1>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Imię i nazwisko</th>
+        <th>Email</th>
+        <th>Telefon</th>
+      </tr>
+  </thead>
+  <tbody>
+    @foreach($patients as $patient)
+      <tr>
+        <td>{{ $patient->first_name }} {{ $patient->last_name }}</td>
+        <td>{{ $patient->user->email }}</td>
+        <td>{{ $patient->phone }}</td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
 @endsection
