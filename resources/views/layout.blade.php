ee32d0d7cefecf58787329657e57d22b7290bc2e
itamm15 <osinski.mateusz15@gmail.com>
2025-06-06 22:30:35 +0200

    chore: basic patients page

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,9 +5,6 @@
   <meta charset="UTF-8">
   <title>Przychodnia</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- For hot reloading -->
-  @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
 <body>
@@ -26,7 +23,7 @@
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link">
+              <a class="nav-link" href="{{ route('patients.index') }}">
                 🩺 Pacjenci
               </a>
             </li>
--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PatientController;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get("/patients", [PatientController::class, 'index'])->name('patients.index');
