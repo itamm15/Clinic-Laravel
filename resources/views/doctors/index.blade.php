c2a81c3ce841c10ce5ca0df3682a4126d5ce7ff3
itamm15 <osinski.mateusz15@gmail.com>
2025-06-28 20:49:57 +0200

    chore: hide buttons on doctors index

--- resources/views/doctors/index.blade.php
+++ resources/views/doctors/index.blade.php
@@ -3,7 +3,10 @@
 @section('content')
   <div class="d-flex justify-content-between align-items-center mb-3">
     <h1>Lista lekarzy</h1>
-    <a href="{{ route('doctors.create') }}" class="btn btn-primary mb-3">Dodaj lekarza</a>
+
+    @if (auth()->user()->is_admin)
+      <a href="{{ route('doctors.create') }}" class="btn btn-primary mb-3">Dodaj lekarza</a>
+    @endif
   </div>
 
   <input type="text" placeholder="Podaj imie lub nazwisko doktora" class="form-control mb-3 w-50" id="searcher">
@@ -25,13 +28,17 @@
           <td>{{ $doctor->user->email }}</td>
           <td>{{ $doctor->phone }}</td>
           <td class="d-flex gap-2">
-            <a href="{{ route('doctors.edit', $doctor->id) }}" class="btn btn-primary btn-sm">Edytuj</a>
+            @if (auth()->user()->is_admin)
+              <a href="{{ route('doctors.edit', $doctor->id) }}" class="btn btn-primary btn-sm">Edytuj</a>
 
-            <form action="{{ route('doctors.delete', $doctor) }}" method="POST" onsubmit="return confirm('Na pewno usunąć?')">
-              @csrf
-              @method('DELETE')
-              <button class="btn btn-sm btn-danger">Usuń</button>
-            </form>
+              <form action="{{ route('doctors.delete', $doctor) }}" method="POST" onsubmit="return confirm('Na pewno usunąć?')">
+                @csrf
+                @method('DELETE')
+                <button class="btn btn-sm btn-danger">Usuń</button>
+              </form>
+            @else
+              Brak akcji
+            @endif
           </td>
         </tr>
       @endforeach
