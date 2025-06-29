6672d8a9c2e3f70614090fee83d50bc0d1587d83
itamm15 <osinski.mateusz15@gmail.com>
2025-06-29 10:04:01 +0200

    chore: hide buttons on procedures index

--- resources/views/procedures/index.blade.php
+++ resources/views/procedures/index.blade.php
@@ -3,7 +3,9 @@
 @section('content')
 <div class="d-flex justify-content-between align-items-center mb-3">
   <h2>Zabiegi</h2>
-  <a href="{{ route('procedures.create') }}" class="btn btn-primary mb-3">Dodaj zabieg</a>
+  @if (!auth()->user()->patient)
+    <a href="{{ route('procedures.create') }}" class="btn btn-primary mb-3">Dodaj zabieg</a>
+  @endif
 </div>
 
 <input type="text" placeholder="Podaj opis zabiegu" class="form-control mb-3 w-50" id="searcher">
@@ -22,13 +24,17 @@
         <td>{{ $procedure->doctor->first_name }} {{ $procedure->doctor->last_name }}</td>
         <td>{{ $procedure->description }}</td>
         <td class="d-flex gap-2">
-          <a href="{{ route('procedures.edit', $procedure->id) }}" class="btn btn-primary btn-sm">Edytuj</a>
+          @if (!auth()->user()->patient)
+            <a href="{{ route('procedures.edit', $procedure->id) }}" class="btn btn-primary btn-sm">Edytuj</a>
 
-          <form action="{{ route('procedures.delete', $procedure->id) }}" method="POST" onsubmit="return confirm('Na pewno usunąć?')">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger btn-sm">Usuń</button>
-          </form>
+            <form action="{{ route('procedures.delete', $procedure->id) }}" method="POST" onsubmit="return confirm('Na pewno usunąć?')">
+              @csrf
+              @method('DELETE')
+              <button type="submit" class="btn btn-danger btn-sm">Usuń</button>
+            </form>
+          @else
+            Brak akcji
+          @endif
         </td>
       </tr>
     @endforeach
